3ee3c3fe69a6a687dc6d0fb9ae4bf4e1f45fee14
h3lwyzx <h3lwyzx@gmail.com>
2024-08-31 02:40:13 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,20 @@
-file koneksi.php
+<?php
+$servername = "localhost";
+$database = "u123456789_DatabaseName";
+$username = "u123456789_User";
+$password = "MyStr0ngPa$!";
+ 
+// Create connection
+ 
+$conn = mysqli_connect($servername, $username, $password, $database);
+ 
+// Check connection
+ 
+if (!$conn) {
+ 
+    die("Connection failed: " . mysqli_connect_error());
+ 
+}
+echo "Connected successfully";
+mysqli_close($conn);
+?>
